c50ff53320efc8b068904a45fc6ae1f8ceb0621f
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-11-16 02:12:36 +0100

    Initial Commit

---
+++ src/main/java/git/newdawn/forgonestructures/ForgoneStructures.java
@@ -0,0 +1,14 @@
+package git.newdawn.forgonestructures;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ForgoneStructures implements ModInitializer {
+	public static final String MOD_ID = "forgone-structures";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() { }
+}
---
+++ src/main/java/git/newdawn/forgonestructures/ForgoneStructuresClient.java
@@ -0,0 +1,10 @@
+package git.newdawn.forgonestructures;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ForgoneStructuresClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
